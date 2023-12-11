476baae044334095873b92ab6491250a28e9e2eb
Andreas Affentranger <andreas.affentranger@sunrise.ch>
2023-12-11 21:18:04 +0000

    fix day10 part01 test

--- day10/day10_test.go
+++ day10/day10_test.go
@@ -6,11 +6,11 @@ import (
 	"github.com/HSLU-Student/AoC-2023/util"
 )
 
-var INPUT = util.GetContentRoot("day09")
+var INPUT = util.GetContentRoot("day10")
 
 func TestPart1(t *testing.T) {
 
-	expect := 1938800261
+	expect := 6907
 	got := Day10{}.Part1(INPUT)
 
 	if got.Result != expect {
